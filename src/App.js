209d0db5d7009f6b2125e2059efeb82df99e609b
Rachel Lau <rachellau@berkeley.edu>
2019-03-06 02:57:05 -0800

    putting in state variables and fetching data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      timeOfDay: ["morning", "afternoon", "evening"],
+      hour: 0,
+      minute: 0,
+      fact: "",
+      quote: ""
+    };
+  }
+
+  async loadAPI() {
+    const factOfDay = await fetch("http://randomuselessfact.appspot.com/today.json?language=en");
+    const jsonFOD = factOfDay.json();
+    const quoteOfDay = await fetch("http://quotes.rest/qod.json")
+    const jsonQOD = quoteOfDay.json();
+    this.setState({
+      fact: jsonFOD.text,
+      quote: jsonQOD.text,
+    })
+  }
+
+  async componentDidMount() {
+    this.loadAPI();
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+
+      // <head>
+      //   <link href="https://fonts.googleapis.com/css?family=Reenie Beanie:regular" rel="stylesheet" type="text/css" />
+      // </head>
+      <body className="App">
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            good morning! here's your daily tea:
+            <br />
+            {this.state.fact}
+            <br /><br /><br /><br />
+            {this.state.quote}
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      </body>
     );
   }
 }
