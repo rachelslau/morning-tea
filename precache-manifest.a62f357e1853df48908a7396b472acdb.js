b722e2526bdc251d90fdce94fb1189ccc5040a33
Rachel Lau <rachellau@berkeley.edu>
2019-03-09 21:04:05 -0800

    Updates

---
+++ precache-manifest.a62f357e1853df48908a7396b472acdb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "04cfcb1e52e6c5832076",
+    "url": "/morning-tea/static/js/runtime~main.04cfcb1e.js"
+  },
+  {
+    "revision": "ede94ca46750dab821d0",
+    "url": "/morning-tea/static/js/main.ede94ca4.chunk.js"
+  },
+  {
+    "revision": "962c62d0053d7663d155",
+    "url": "/morning-tea/static/js/2.962c62d0.chunk.js"
+  },
+  {
+    "revision": "ede94ca46750dab821d0",
+    "url": "/morning-tea/static/css/main.9a6819f4.chunk.css"
+  },
+  {
+    "revision": "1658f71c4469ee3da0237463442a0c3e",
+    "url": "/morning-tea/index.html"
+  }
+];
